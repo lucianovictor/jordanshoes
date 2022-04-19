0f7f03fa201bfbae02c03446495be76f73f078d7
Luciano <54756923+lucianovictor@users.noreply.github.com>
2022-04-19 14:55:00 -0300

    :sparkles: Finish Footer and styles

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export const Footer = () => {
+  return (
+    <footer className={styles.container}>
+      <h2>Todos os direitos reservados.</h2>
+    </footer>
+  )
+}
