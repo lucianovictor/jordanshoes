4b20bcc58a08fec839696a133c195c8ff231e940
Luciano <54756923+lucianovictor@users.noreply.github.com>
2022-04-19 14:56:24 -0300

    :sparkles: Finish Sneakers and styles

---
+++ src/components/Sneakers/index.tsx
@@ -0,0 +1,34 @@
+import styles from './styles.module.scss'
+
+export const Sneakers = () => {
+  return (
+    <div className={styles.container}>
+      <ul className={styles.content}>
+        <li>
+          <img src="/images/1.png" alt="" />
+        </li>
+        <li>
+          <img src="/images/2.png" alt="" />
+        </li>
+        <li>
+          <img src="/images/3.png" alt="" />
+        </li>
+        <li>
+          <img src="/images/4.png" alt="" />
+        </li>
+        <li>
+          <img src="/images/5.png" alt="" />
+        </li>
+        <li>
+          <img src="/images/6.png" alt="" />
+        </li>
+        <li>
+          <img src="/images/7.png" alt="" />
+        </li>
+        <li>
+          <img src="/images/8.png" alt="" />
+        </li>
+      </ul>
+    </div>
+  )
+}
